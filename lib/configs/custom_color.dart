7f70510ca4b1aba6f8000a6e35fdb31f2f74e7e6
bodii <1401097251@qq.com>
2023-07-17 10:57:26 +0800

    add: no login page style

--- lib/configs/custom_color.dart
+++ lib/configs/custom_color.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  static const Color paimary = Color(0xFF2EFAB4);
-  static const Color secondary = Color(0xFF27EBBB);
+  static const Color paimary = Color(0xFF27EBBB);
+  static const Color secondary = Color(0xFF2EFAB4);
   static const Color tertiary = Color(0xFFA7A7A7);
   static const Color neutral = Color(0xFFFF2753);
   static const Color icon = Color(0xFF01EA97);
--- lib/pages/home/views/no_login_page.dart
+++ lib/pages/home/views/no_login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_douban_fm_clone/configs/custom_color.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class NoLoginPage extends StatelessWidget {
   const NoLoginPage({Key? key}) : super(key: key);
@@ -10,21 +9,59 @@ class NoLoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('no_login')),
       body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/icons/music_symbol01.png',
-                width: 100,
-                height: 100,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    offset: const Offset(10, 10),
+                    blurRadius: 20,
+                    spreadRadius: 20,
+                  ),
+                ],
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
               ),
-            ],
-          ),
+              width: 280,
+              height: 330,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/icons/music_symbol01.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                  const Text('登录后才可进行更多操作'),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: CustomColors.paimary),
+                      foregroundColor: CustomColors.secondary,
+                    ),
+                    onPressed: () {
+                      debugPrint('go login');
+                    },
+                    child: const Text(
+                      '立即登录',
+                      style: TextStyle(
+                        color: CustomColors.paimary,
+                        fontSize: 15,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                debugPrint('关闭');
+              },
+              icon: const Icon(Icons.close, size: 30),
+            ),
+          ],
         ),
       ),
     );
